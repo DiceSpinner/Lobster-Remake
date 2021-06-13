6a56b1c85b08e87e4ee82617bd6cd0106249c001
DiceSpinner <dicespinner1228@gmail.com>
2021-06-12 22:44:42 -0400

    Laid out some of the basic structures of the game.

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import pygame
-from physics import CollisionDetector
+from physics import *
 from typing import Tuple, Union, List
 
 
@@ -9,43 +9,17 @@ class Level:
 
     === Public Attributes ===
     setting
+    _asset: Loaded assets of the game
+    _asset_location: The locations of game assets
+    _physics: Sets of physics rules supported by this level
     """
-
-
-class Game:
-    """
-    Description:
-
-    === Private Attributes ===
-        _asset_location: The locations of game assets
-        _screen: Screen of the game that gets displayed to the player
-        _screen_size: size of the screen
-        _name: Name of the game
-        _asset: Loaded assets of the game
-        _levels: Levels of this game
-    """
-    _asset_location: List[str]
-    _screen: pygame.Surface
-    _screen_size: Tuple[int, int]
     _asset: dict[str, dict[str, Union[pygame.Surface, pygame.mixer.Sound, str]]]
-    _levels: List[Level]
+    _asset_location: List[str]
 
-    def __init__(self, name: str, asset: [str],
-                 screen_size: Tuple[int, int]) -> None:
+    def __init__(self, asset: [str]):
         self._asset_location = asset
-        self._name = name
-        self._screen_size = screen_size
-
-    def start(self):
-        pygame.init()
-        pygame.display.set_icon(pygame.image.load("Lobster_clean.png"))
-        self._screen = pygame.display.set_mode(self._screen_size)
-        pygame.display.set_caption(self._name)
-
-        self._load_assets()
 
     def _load_assets(self) -> None:
-        pygame.mixer.init()
         sound_format = ['wav']
         image_format = ['png', 'jpg', 'gif', 'bmp', 'tif']
         music_format = ['mp3', 'mp4']
@@ -59,4 +33,54 @@ class Game:
             elif suffix in music_format:
                 self._asset['music'][data[0]] = data[1]
 
+    def run(self, screen: pygame.Surface):
+        """
 
+        :return:
+        """
+        running = True
+        while running:
+            a = 1
+
+
+class Game:
+    """
+    Description:
+
+    === Private Attributes ===
+        _screen: Screen of the game that gets displayed to the player
+        _screen_size: size of the screen
+        _name: Name of the game
+
+        _levels: Levels of this game
+    """
+
+    _screen: pygame.Surface
+    _screen_size: Tuple[int, int]
+    _levels: List[Level]
+
+    def __init__(self, name: str,
+                 screen_size: Tuple[int, int]) -> None:
+        self._name = name
+        self._screen_size = screen_size
+
+    def start(self) -> None:
+        """
+            Description: Start the game
+        """
+        pygame.init()
+        pygame.display.set_icon(pygame.image.load("Lobster_clean.png"))
+        self._screen = pygame.display.set_mode(self._screen_size)
+        pygame.display.set_caption(self._name)
+        pygame.mixer.init()
+
+        self.load_level()
+
+    def load_level(self):
+        """
+
+        :return:
+        """
+
+    def display_menu(self):
+        return
--- physics.py
+++ physics.py
@@ -1,5 +1,33 @@
+import pygame
+from typing import List, Any, Tuple, Union
 
-class CollisionDetector:
-    """
+
+class PhysicsApplicator:
 
     """
+    Description: Applicators that apply physics rules
+
+    === Public Attributes ===
+    subjects: Entities subject to this physics rule
+    """
+    subjects: str
+
+    def apply_physics(self, optional: List[Any]) -> Any:
+        raise NotImplementedError
+
+
+class CollisionApplicator(PhysicsApplicator):
+    """
+    Description: Collision rule applicator
+
+    === Public Attributes ===
+    subjects: Entities subject to this physics rule
+    """
+    subjects: []
+
+    def __init__(self):
+        self.subjects = ['collidable']
+
+    def apply_physics(self, optional: List[Any]) -> Any:
+        raise NotImplementedError
+
